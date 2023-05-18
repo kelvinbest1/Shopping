dcca432fbf2ddae7e35758a8750771e9adf88d04
kelvinbest1 <kelvinlanier@yahoo.com>
2023-05-18 07:26:25 -0400

    update Product.js

---
+++ models/Product.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please Enter product Name"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, "Please Enter product Description"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please Enter product Price"],
+    maxLength: [8, "Price cannot exceed 8 characters"],
+  },
+ 
