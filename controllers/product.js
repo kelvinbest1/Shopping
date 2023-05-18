a1d4accd0e3a0429d4032860aeee30273212aae9
kelvinbest1 <kelvinlanier@yahoo.com>
2023-05-18 07:40:24 -0400

    add getAdminProducts

--- controllers/product.js
+++ controllers/product.js
@@ -28,3 +28,13 @@ createProduct = catchAsyncError(async (req, res, next) => {
       resultsPerPage,
     });
   });
+
+ getAdminProducts = catchAsyncError(async (req, res, next) => {
+    const products = await Product.find();
+  
+    res.status(200).json({
+      success: true,
+      products,
+    });
+  });
+  
