7c4927265db2803b749d21472f1cce561644b94e
kelvinbest1 <kelvinlanier@yahoo.com>
2023-05-18 07:41:59 -0400

    add update function

--- controllers/product.js
+++ controllers/product.js
@@ -49,5 +49,23 @@ createProduct = catchAsyncError(async (req, res, next) => {
       product,
     });
   });
+
+  updateProduct = catchAsyncError(async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
+    if (!product) {
+      return next(new ErrorHander("Product not found", 404));
+    }
+  
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+  
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  });
   
   
