8ba2276f521cfdfe6471e596921bc8d0a62153d9
kelvinbest1 <kelvinlanier@yahoo.com>
2023-05-18 07:38:34 -0400

    add createProduct function

--- controllers/product.js
+++ controllers/product.js
@@ -3,3 +3,12 @@ const ErrorHander = require("../utils/errorHandler");
 const catchAsyncError = require("../middleware/catchAcyncError");
 const ApiFeatures = require("../utils/apiFeatures");
 const cloudinary = require("cloudinary");
+
+createProduct = catchAsyncError(async (req, res, next) => {
+
+    const product = await Product.create(req.body);
+    res.status(201).json({
+      success: true,
+      product,
+    });
+  });
